cf1be380d8851c2981dd55e7eb5b538c388a00b9
learnerpat <466260pat@gmail.com>
2019-10-13 18:16:50 -0400

    calcualte avg of reviews

--- read.py
+++ read.py
@@ -1,7 +1,16 @@
 data = []
+count = 0
 with open("reviews.txt","r") as f:
 	for line in f:
 		data.append(line)
-print(len(data))
-print(data[0]+"\n")
-print(data[1])
+		count += 1
+		if count % 100000 == 0:
+			print(len(data))
+print("总共有", len(data), "条留言.")
+	
+sun_len = 0
+for message in data:
+	sun_len=sun_len + len(message)
+print("流言的平均长度是: ", sun_len/len(data))
+
+
